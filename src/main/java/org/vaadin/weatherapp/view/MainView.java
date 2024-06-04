405e798010122173febb39356edd8b3d4166eb96
mazhar <mazhar.zahur@infolytx.com>
2024-06-04 11:42:27 +0600

    add project structure

---
+++ src/main/java/org/vaadin/weatherapp/view/MainView.java
@@ -0,0 +1,14 @@
+package org.vaadin.weatherapp.view;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+
+    }
+
+}
